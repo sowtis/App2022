334ac44a901cc698d74a068d1ada00e28ae2cef3
Dylan Sowtis <dsowtis@gmail.com>
2022-03-23 01:40:21 -0600

    pages

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App2022.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
